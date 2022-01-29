5bbe565517d9a1569a29246a2f4a4949abc11abc
John <jmiclat@ufl.edu>
2022-01-29 17:12:21 -0500

    login form with functioning API calls

---
+++ frontend/src/App.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {observer} from 'mobx-react';
+import UserStore from './stores/UserStore';
+import LoginForm from './LoginForm';
+import InputField from './InputField';
+import SubmitButton from './SubmitButton';
+import './App.css';
+
+class App extends React.Component {
+
+  //Define API calls
+
+  async componentDidMount()
+  {
+    try{
+      let res = await fetch('/isLoggedIn', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+
+      let result = await res.json();
+
+      if(result && result.success) {
+        UserStore.loading = false;
+        UserStore.isLoggedIn = true;
+        UserStore.username = result.username;
+      }
+
+      else{
+        UserStore.loading = false;
+        UserStore.isLoggedIn = false;
+      }
+
+    }
+    catch(e){
+      UserStore.loading = false;
+      UserStore.isLoggedIn = false;
+    }
+  }
+
+
+  //Define Logout Function
+  async doLogout()
+  {
+    try{
+      let res = await fetch('/logout', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+
+      let result = await res.json();
+
+      if(result && result.success) {
+        UserStore.isLoggedIn = false;
+        UserStore.username = '';
+      }
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+
+  render() {
+
+    if(UserStore.loading) {
+      return (
+        <div className='App'>
+          <div className='=container'>
+            Loading, please wait...
+          </div>
+        </div>
+
+      );
+    }
+
+    else {
+      
+      if(UserStore.isLoggedIn) {
+        return(
+          <div className='=App'>
+            <div className='container'>
+              Welcome{UserStore.username}
+
+              <SubmitButton
+                text={'Log out'}
+                disabled={false}
+                onClick={ ()=>this.doLogout() }
+              />
+            </div>
+          </div>
+
+        );
+      }
+    }
+    return(
+      <div className="App">
+
+        <LoginForm />
+      </div>
+    );
+  }
+}
+
+export default observer(App);
---
+++ frontend/src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+        return (
+        <React.Fragment>
+            <h1>Fridge Master</h1>
+            <button>Login</button>
+        </React.Fragment>
+        );
+    }
+}
+ 
+export default Counter;
