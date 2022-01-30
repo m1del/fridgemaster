d01b88c575134095e8a59425860b12b83fc0a5b7
dyannya <janiarfernan@gmail.com>
2022-01-30 07:21:39 -0500

    error fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,62 @@
-import React, {useState, useEffect} from 'react'
-import './App.css';
-import Form from './components/Form';
+import React from 'react';
+import { AppBar, Toolbar, Grid } from '@material-ui/core';
 import logo from './images/fridge-logo.png';
 import fridge from './images/fridge-image.png';
-import { makeStyles, Typography, AppBar, Card, CardActions, 
-    CardContent, CardMedia, CssBaseline, 
-    Grid, Toolbar, Container } 
-    from '@material-ui/core';
-
+import Form from './/components/Form'
 
 console.log(logo);
 console.log(fridge);
 
+
+
+const myStyle = {
+    backgroundColor: '#616265',
+    height: '100.5vh',
+    marginTop: '-10px',
+    marginBottom: '-10px',
+    marginLeft: '-10px',
+    marginRight: '-10px',
+    backgroundSize: 'cover',
+};
+
 function DisplayLogo() {
     return <img src={logo}
-            style = {{ 
-                position: 'relative',
-                top: '5px',
-                left: '30px'}}/>;
+            style = {{ position: 'relative',
+                        top: '30px',
+                        left: '30px'}}/>;
 }
 
 function DisplayFridge() {
-    return <img src = {fridge}/>;
+    return <img src = {fridge}
+            style = {{ display: 'flex',
+                    justifyContent: 'center'}}/>;
 }
 
-function App() {
-
-    const myStyle = {
-        backgroundColor: '#616265',
-        position: 'relative',
-        height: '100.5vh',
-        marginTop: '-10px',
-        marginBottom: '-10px',
-        marginLeft: '-10px',
-        marginRight: '-10px',
-        backgroundSize: 'cover',
-    };
-
-  return (
-    <div className="App"
-        style = {myStyle}>
-            <AppBar position = "relative"
+const App = () => {
+    return (
+        <div 
+            className="App"
+            style = {myStyle}>
+                <AppBar 
+                    position = "relative"
                     style = {{height: "10%",
-                            backgroundColor: "#616265"}}>
-                <Toolbar>
-                    <DisplayLogo/>
-                </Toolbar>
-            </AppBar>
-        <div style = {{justifyContent: 'center'}}>
-            <Grid container spacing={1}>
-                <Grid item xs={6} md={6}>
-                    <DisplayFridge/>
+                    backgroundColor: "#616265"}}>
+                        <Toolbar
+                            position = "relative">
+                                <DisplayLogo/>
+                        </Toolbar>
+                </AppBar>
+            <div>
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={6}>
+                        <DisplayFridge/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Form/>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6} md={2}>
-                    <Form />
-                </Grid>
-            </Grid>
+            </div>
         </div>
-    </div>
-  );
-};
-
+    );
+}
 export default App;
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import FormSignup from './FormSignup'
 import FormSuccess from './FormSuccess'
-import './Form.css'
+import './Form'
 
 const Form = () => {
     const [isSubmitted, setIsSubmitted] = useState(false);
